65ca5c7a50faad8e6f587f48a9a6271ff3da2bff
Ala_ALsalam <alaalsalam101@gmail.com>
2022-01-27 00:22:01 +0300

    creat Django fail with view and templeats

---
+++ pages/views.py
@@ -0,0 +1,13 @@
+import re
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def index(request):
+    return render(request,'pages/index.html',{'name':'ALa'})
+    # return HttpResponse("Ala HI dfdsfasdf")
+
+    
+
+# Create your views here.
