940610c5613352c8673dfd25383940e793dabea8
ALa101 <59401688+ALa101@users.noreply.github.com>
2022-02-07 21:44:38 +0300

    test project

--- cosmetic/brand/models.py
+++ cosmetic/brand/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Brand(models.Model):
     Name = models.CharField(max_length=20)
     Orgin = models.CharField(max_length=50)
-    image = models.ImageField(upload_to="image/brand/%y/%m/%d", height_field=600, width_field=400,null = True, blank = True)
+    image = models.ImageField(upload_to="image/brand/%y/%m/%d",null = True, blank = True)
     
 
     def __str__(self):
